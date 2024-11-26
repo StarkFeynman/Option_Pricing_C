c2b2f17c521e09187bb8df00ddd40a800c7435b3
Mohamed Amine Mjaidila <aminemjaidila@gmail.com>
2024-11-26 20:42:30 +0100

    Initialisation

---
+++ Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Company
+{
+	public Company()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
